e31ece0cc7ac8f21b60721af4089d932bbaa1b0a
tranhuyland <huy07kt@gmail.com>
2026-05-28 14:28:45 +0700

    Add files via upload

---
+++ services/properties.ts
@@ -0,0 +1,12 @@
+export async function getProperties() {
+  return [
+    {
+      id: 1,
+      title: 'Nhà 3 tầng Hòa Xuân',
+      price: '4.5 tỷ',
+      location: 'Đà Nẵng',
+      image:
+        'https://images.unsplash.com/photo-1600585154526-990dced4db0d'
+    }
+  ]
+}
