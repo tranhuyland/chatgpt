c4b466c9b09d69bbf3b6541c46ec993dd8b18b7d
tranhuyland <huy07kt@gmail.com>
2026-05-28 14:27:33 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import './globals.css'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  )
+}
